104c435e2c071f4fe973b909eff934a0b10220f7
Byte <captain11248@gmail.com>
2026-02-23 23:51:51 +0330

    variable.c

---
+++ C/variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    // int cant to be a float number
+    int age = 15;
+    int year = 2026;
+
+    printf("you are %d years old\n", age);
+    printf("the year is %d", year);
+
+    return 0;
+}
