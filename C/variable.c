48c9211f96cdddea8ca5838faa391346f909921d
Byte <captain11248@gmail.com>
2026-02-24 23:22:10 +0330

    Add files via upload

--- C/variable.c
+++ C/variable.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
+    
+    // int age = 14;
+    // int year = 2026;
 
-    // int cant to be a float number
-    int age = 15;
-    int year = 2026;
+    // printf("you are %d years old\n", age);
+    // printf("the year is %d", year);
 
-    printf("you are %d years old\n", age);
-    printf("the year is %d", year);
+    // float score = 3.5;
+    // float price = 19.99;
+
+    // printf("your score is %f\n", score);
+    // printf("the price is $%.2F", price);
+
+    // double pi = 3.141592653589793;
+    // printf("the value of pi is %.15lf", pi);
+
+    // char word = 'a';
+    // printf("word is %c", word);
+
+    // char name[] = "daniel";
+    // printf("Hello %s", name);
+
+    bool isONLINE = false;
+
+    if(isONLINE){
+        printf("you are online");
+    }
+    else{
+        printf("you are offline");
+    }
 
     return 0;
 }
